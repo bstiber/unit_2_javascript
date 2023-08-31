32713232866578bfff4c372d8ec63cfb3827a017
Bill Stiber <billstiber@gmail.com>
2023-08-31 12:03:23 -0400

    first commit

---
+++ challenge.js
@@ -0,0 +1,30 @@
+//Write a program that declares a variable name and assigns it a 
+//string value of your first name. 
+//Then, print the value of the name variable to the console.
+let firstName = 'Bill';
+//console.log(firstName)
+
+//Write a program that declares a variable age and assigns it a number value of your age. Then, print the value of the age variable to the console.
+let age = '51';
+//console.log(age);
+
+//Write a program that declares a variable isStudent and assigns it a boolean value of true or false depending on whether or not you are currently a student. Then, print the value of the isStudent variable to the console.
+let isStudent = false;
+//console.log(isStudent);
+
+//Write a program that declares a variable favoriteColors and assigns it an array of string values representing your favorite colors. Then, print the value of the favoriteColors variable to the console.
+//let array = ['red', 'blue', 'green'];
+//console.log(favoriteColors);
+
+
+let favoriteColors = ['red', 'blue', 'green'];
+//console.log(favoriteColors);
+
+//Write a program that declares a variable person and assigns it an object with properties for name, age, and isStudent. Assign values to each property based on your own personal information. Then, print the value of the person variable to the console.
+let person = {
+  name : 'Bill',
+  age : 51,
+  isStudent : false,
+}
+
+console.log(person);
---
+++ variable.js
@@ -0,0 +1,17 @@
+let age = 27;
+let isDoorClosed = true;
+let fullName = 'John Doe';
+let time = null;
+let x;
+
+let person = {
+  firstName : 'John',
+  lastName : 'Doe',
+  age : 27
+}
+
+let cities = ['london', 'Atlanta', 'San Diego',]
+
+console.log(typeof cities);
+
+
